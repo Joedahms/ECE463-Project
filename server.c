8b2370d1548205b3775b5a51a6e966c8e837cafa
Joedahms <dahms.joejoe@gmail.com>
2024-08-22 18:39:37 -0400

    Learning about using getaddrinfo() to set up server

--- server.c
+++ server.c
@@ -1,5 +1,24 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+
 #include "server.h"
 
 int main(void) {
+
+	int status;
+	struct addrinfo hints;
+	struct addrinfo* serverAddressInfo;
+
+	hints.ai_family = AF_INET;		// IPV4
+	hints.ai_socktype = SOCK_STREAM;	// TCP
+	hints.ai_protocol = 0;			// Any protocol
+	hints.ai_flags = AI_PASSIVE;		// Socket address suitable for binding a socket that will accept connections (if node is null)
+	
+	getaddrinfo(NULL, "3940", &hints, &serverAddressInfo);
+	
+	freeaddrinfo(serverAddressInfo);
+
 	return 0;
 }
